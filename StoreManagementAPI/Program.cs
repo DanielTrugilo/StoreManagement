c9644bfdc25cf813c23a18967f656ffb6907d7b9
DanielTrugilo <dvat03@gmail.com>
2025-08-29 19:58:22 -0300

    Test

--- StoreManagementAPI/Program.cs
+++ StoreManagementAPI/Program.cs
@@ -6,7 +6,7 @@ using StoreManagementAPI.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
